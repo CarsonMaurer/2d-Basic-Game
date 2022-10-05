fa80ccde4f3b53c804e7fe37cf3bf37015295c81
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-10-05 11:01:25 -0700

    finished shooting mech

--- 2D Basic Game/Assets/Scipts/destoryOutOfBounds.cs
+++ 2D Basic Game/Assets/Scipts/destoryOutOfBounds.cs
@@ -15,5 +15,9 @@ public class destoryOutOfBounds : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        if(transform.position.y < lowerBound)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
